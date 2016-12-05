7d7d8fc8af8c17a654daf80d2fcb2f2cc2c8e099
Harald van der Laan <harald@familievdlaan.nl>
2016-12-05 07:52:19 +0100

    update preprocess for messages ending with a k

--- p2000.py
+++ p2000.py
@@ -79,6 +79,7 @@ def main():
             msg = re.sub(r'd\[', 'd\n[', msg)
             msg = re.sub(r't\[', 't\n[', msg)
             msg = re.sub(r'e\[', 'e\n[', msg)
+            msg = re.sub(r'k\[', 'k\n[', msg)
 
             p2000msg = p2000.PreprocessMessage(msg)
             p2000.DisplayMessage(p2000msg.msgtype, msg)
